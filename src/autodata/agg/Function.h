ec1d41e05a5f20c73df2c7c6596e0346963a86b9
Joshua Koch <jbkoch@agsolver.com>
2015-07-29 13:05:28 -0500

    Add some helper functions for variadic aggregation

---
+++ src/autodata/agg/Function.h
@@ -0,0 +1,130 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * Copyright 2012-2013 by Ames Laboratory
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+
+#pragma once
+
+// --- AutoData Includes --- //
+#include <autodata/AutoDataConfig.h>
+#include <autodata/CompilerGuards.h>
+
+namespace autodata
+{
+namespace agg
+{
+
+///
+int vcount()
+{
+    return 0;
+}
+
+///
+template< typename T, typename... Ts >
+int vcount(
+    T&& v,
+    Ts&&... vs )
+{
+  return 1 + vcount( std::forward< Ts >( vs )... );
+}
+
+///
+template< typename T, typename =
+    typename std::enable_if< std::is_arithmetic< T >::value >::type >
+T vsum(
+    T v )
+{
+    return v;
+}
+
+///
+template< typename T, typename... Ts, typename =
+    typename std::enable_if< std::is_arithmetic< T >::value >::type >
+auto vsum(
+    T v,
+    Ts... vs ) -> decltype( v + vsum( vs... ) )
+{
+    return v + vsum( vs... );
+}
+
+///
+template< typename T, typename =
+    typename std::enable_if< std::is_arithmetic< T >::value >::type >
+T vavg(
+    T v )
+{
+    return v;
+}
+
+///
+template< typename T, typename... Ts, typename =
+    typename std::enable_if< std::is_arithmetic< T >::value >::type >
+double vavg(
+    T v,
+    Ts... vs )
+{
+    return vsum( v, vs... ) / (double)vcount( v, vs... );
+}
+
+///
+template< typename T >
+T&& vmin(
+    T&& v )
+{
+    return std::forward< T >( v );
+}
+
+template< typename T0, typename T1, typename... Ts >
+auto vmin(
+    T0&& v0,
+    T1&& v1,
+    Ts&&... vs ) -> decltype(
+        ( v0 < v1 ) ?
+            vmin( v0, std::forward< Ts >( vs )... ) :
+            vmin( v1, std::forward< Ts >( vs )... ) )
+{
+    return ( v0 < v1 ) ?
+      vmin( v0, std::forward< Ts >( vs )... ) :
+      vmin( v1, std::forward< Ts >( vs )... );
+}
+
+///
+template< typename T >
+T&& vmax(
+    T&& v )
+{
+    return std::forward< T >( v );
+}
+
+template< typename T0, typename T1, typename... Ts >
+auto vmax(
+    T0&& v0,
+    T1&& v1,
+    Ts&&... vs ) -> decltype(
+        ( v0 > v1 ) ?
+            vmax( v0, std::forward< Ts >( vs )... ) :
+            vmax( v1, std::forward< Ts >( vs )... ) )
+{
+    return ( v0 > v1 ) ?
+      vmax( v0, std::forward< Ts >( vs )... ) :
+      vmax( v1, std::forward< Ts >( vs )... );
+}
+
+} // end agg
+} // end autodata
---
+++ src/tests/agg_test.cxx
@@ -0,0 +1,67 @@
+
+// --- App Includes --- //
+#include <autodata/agg/Function.h>
+
+using namespace autodata;
+
+// --- STL Includes --- //
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+////////////////////////////////////////////////////////////////////////////////
+void Main()
+{
+    auto sum1 = agg::vsum( 3, 4.6f );
+    auto sum2 = agg::vsum( 4, 5.2f, 7.0 );
+    std::cout
+        << "sum1: " << sum1 << " | type1: "
+        << typeid( decltype( sum1 ) ).name() << std::endl;
+    std::cout
+        << "sum2: " << sum2 << " | type2: "
+        << typeid( decltype( sum2 ) ).name() << std::endl;
+
+    auto avg1 = agg::vavg( 3, 4.6f );
+    auto avg2 = agg::vavg( 4, 5.2f, 7.0 );
+    std::cout
+        << "avg1: " << avg1 << " | type1: "
+        << typeid( decltype( avg1 ) ).name() << std::endl;
+    std::cout
+        << "avg2: " << avg2 << " | type2: "
+        << typeid( decltype( avg2 ) ).name() << std::endl;
+
+    auto min1 = agg::vmin< std::string >( "a", "abc", "ab" );
+    std::cout
+        << "min1: " << min1 << " | type1: "
+        << typeid( decltype( min1 ) ).name() << std::endl;
+
+    auto max1 = agg::vmax( 3, 4.6f, 1 );
+    auto max2 = agg::vmax( 4, 5.2f, 2.0 );
+    std::cout
+        << "max1: " << max1 << " | type1: "
+        << typeid( decltype( max1 ) ).name() << std::endl;
+    std::cout
+        << "max2: " << max2 << " | type2: "
+        << typeid( decltype( max2 ) ).name() << std::endl;
+}
+////////////////////////////////////////////////////////////////////////////////
+int main(
+    int argc,
+    char** argv)
+{
+    try
+    {
+        Main();
+    }
+    catch( std::exception const& e )
+    {
+        std::cerr << "*** std::exception caught: "
+            << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+////////////////////////////////////////////////////////////////////////////////
