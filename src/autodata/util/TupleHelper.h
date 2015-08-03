05894a3fa76afa734e0f8eeeeff002908f681ee7
Joshua Koch <jbkoch@agsolver.com>
2015-08-03 12:07:53 -0500

    Add some helper functions for working w/ tuples and parameter packs

---
+++ src/autodata/util/TupleHelper.h
@@ -0,0 +1,93 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * Copyright 2012-2013 by Ames Laboratory
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+
+#pragma once
+
+// --- AutoData Includes --- //
+#include <autodata/AutoDataConfig.h>
+#include <autodata/CompilerGuards.h>
+
+// --- Standard Includes --- //
+#include <functional>
+#include <tuple>
+#include <utility>
+
+namespace autodata
+{
+namespace util
+{
+
+///c++14: std::integer_sequence/std::index_sequence
+
+///
+template< std::size_t... >
+struct seq{ using type = seq; };
+
+///
+template< std::size_t N, std::size_t... Ns >
+struct gens : gens< N - 1, N - 1, Ns... >{};
+
+///
+template< std::size_t... Ns >
+struct gens< 0, Ns... > : seq< Ns... >{};
+
+///Reversed
+template< std::size_t N, std::size_t... Ns >
+struct rgens : rgens< N - 1, Ns..., N - 1 >{};
+
+///Reversed
+template< std::size_t... Ns >
+struct rgens< 0, Ns... > : seq< Ns... >{};
+
+///
+struct tuple_unpacker
+{
+    ///Value returning function
+    template< typename Callable, typename Tuple, std::size_t... Ns >
+    static auto apply(
+        Callable&& fn,
+        Tuple&& tup,
+        seq< Ns... > ) -> decltype(
+            fn( std::get< Ns >( std::forward< Tuple >( tup ) )... ) )
+    {
+        return fn( std::get< Ns >( std::forward< Tuple >( tup ) )... );
+    }
+};
+
+///
+template< typename Unpacker, typename Callable, typename Tuple >
+auto apply_tuple(
+    Callable&& fn,
+    Tuple&& tup ) -> decltype(
+        Unpacker::apply(
+            std::forward< Callable >( fn ),
+            std::forward< Tuple >( tuple ),
+            typename gens< std::tuple_size<
+                typename std::remove_reference< Tuple >::type >::value >::type() ) )
+{
+    return Unpacker::apply(
+        std::forward< Callable >( fn ),
+        std::forward< Tuple >( tuple ),
+        typename gens< std::tuple_size<
+            typename std::remove_reference< Tuple >::type >::value >::type() );
+}
+
+} //end util
+} //end autodata
