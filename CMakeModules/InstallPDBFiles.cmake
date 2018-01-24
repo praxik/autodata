464855dd8e42ffc3ed241ff9e408c09a54979370
Joshua Koch <jbkoch@agsolver.com>
2018-01-24 16:31:23 -0600

    Fix python bindings on windows.

--- CMakeModules/InstallPDBFiles.cmake
+++ CMakeModules/InstallPDBFiles.cmake
@@ -2,13 +2,22 @@
 #On windows provide the user with the pdb files for debugging if they are present
 if( MSVC )
     #Debug symbols are only created for executable and shared library targets
-    get_target_property( TARGET_TYPE ${TARGET_NAME} TYPE )
+    if( ${TARGET_CATEGORY} STREQUAL "Swig" )
+        get_target_property( TARGET_TYPE ${SWIG_MODULE_${TARGET_NAME}_REAL_NAME} TYPE )
+        get_target_property( PDB ${SWIG_MODULE_${TARGET_NAME}_REAL_NAME} PDB_NAME )
+    else()
+        get_target_property( TARGET_TYPE ${TARGET_NAME} TYPE )
+        get_target_property( PDB ${TARGET_NAME} PDB_NAME )
+    endif()
     if( ${TARGET_TYPE} STREQUAL "STATIC_LIBRARY" )
         return()
     endif()
-    get_target_property( PDB ${TARGET_NAME} PDB_NAME )
     if( ${PDB} STREQUAL "PDB-NOTFOUND" )
-        set( PDB ${TARGET_NAME} )
+        if( ${TARGET_CATEGORY} STREQUAL "Swig" )
+            set( PDB ${SWIG_MODULE_${TARGET_NAME}_REAL_NAME} )
+        else()
+            set( PDB ${TARGET_NAME} )
+        endif()
     endif()
     install(
         FILES ${CMAKE_CURRENT_BINARY_DIR}/\${CMAKE_INSTALL_CONFIG_NAME}/${PDB}.pdb
